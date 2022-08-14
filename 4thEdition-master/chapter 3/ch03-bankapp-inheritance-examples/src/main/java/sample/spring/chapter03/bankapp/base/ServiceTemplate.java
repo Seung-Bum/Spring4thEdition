8523829cf62b4e6c0fd07d0a3e09f843c81a8528
Seung-Bum <sb910126@gmail.com>
2022-08-15 00:32:34 +0900

    빈 정의 상속

--- 4thEdition-master/chapter 3/ch03-bankapp-inheritance-examples/src/main/java/sample/spring/chapter03/bankapp/base/ServiceTemplate.java
+++ 4thEdition-master/chapter 3/ch03-bankapp-inheritance-examples/src/main/java/sample/spring/chapter03/bankapp/base/ServiceTemplate.java
@@ -1,5 +1,8 @@
 package sample.spring.chapter03.bankapp.base;
 
+// serviceTemplate 빈은 userRequestController 빈의 의존 관계다.
+// 부모 빈 정의가 추상이 아닌 경우 그 부모 빈이 제공하는 기능을 자식 빈뿐 아니라 XML 파일에 있는 다른 빈도 사용 할 수 있다.
+
 public class ServiceTemplate {
 	private JmsMessageSender jmsMessageSender;
 	private EmailMessageSender emailMessageSender;
